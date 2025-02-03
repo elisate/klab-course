058398bac2a622c226b1065bb968b525752b8c83
muhire <dushimiyimanaelisa@gmail.com>
2025-02-03 22:16:04 +0200

    fix change

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import "../styles/model.css";
-import { IoMdClose } from "react-icons/io";
+
 
 function ModelForm() {
   return (
     <div className="overlay">
       <form className="model">
-        <div className="header">
-          {" "}
-          <div>Register</div>
-          <div><IoMdClose/></div>
-        </div>
+        
         <input type="text" placeholder="Enter your name" className="input" />
         <input type="email" placeholder="Enter your email" className="input" />
         <button type="button" className="button">
--- src/components/ModelForm.jsx
+++ src/components/ModelForm.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import '../styles/model.css';
+import { IoMdClose } from "react-icons/io";
 function ModelForm() {
   return (
     <div className="overlay">
       <form className="model">
+        <div className="header">
+          <div>Register</div>
+          <div>
+            <IoMdClose />
+          </div>
+        </div>
         <input type="text" placeholder="names" className="input" />
         <input type="email" placeholder="email" className="input" />
         <button type="button" className="button">
